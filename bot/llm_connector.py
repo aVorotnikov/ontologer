af261974199a8cac92e6e58a109e7443ac23c220
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2024-12-01 20:16:32 +0300

    ДОбавлен коннектор с LLM

---
+++ bot/llm_connector.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from ollama import chat
+from ollama import ChatResponse
+
+
+class LlmConnector:
+    def __init__(self):
+        self.model = "qwen2:latest"
+
+
+    def generate_response(self, message):
+        response = chat(model=self.model, messages=[{
+            "role": "user",
+            "content": message
+        }])
+        return response.message.content
+
+
+if __name__ == "__main__":
+    connector = LlmConnector()
+    print(connector.generate_response(
+        'Ответь односложно: "Да" или "Нет". '
+        'Эквиваленты ли 2 утверждения "Элемент множества - это часть множества" и "Множество состоит из элементов множества"?'))
