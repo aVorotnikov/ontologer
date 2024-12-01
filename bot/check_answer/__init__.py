4330239a7f96af06ce71735436d36a81994e43eb
Andrey Vorotnikov <AndrewVorotnikov@yandex.ru>
2024-12-01 21:07:46 +0300

    Модуль проверки ответов

---
+++ bot/check_answer/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+
+from check_answer import check_answer
---
+++ bot/check_answer/check_answer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+from construct_sequence import construct_sequence
+from llm_connector import LlmConnector
+from ontologies_connector import OntologiesConnector
+
+
+MIN_LENGTH_TO_CHECK = 1
+MAX_LENGTH_TO_CHECK = 3
+
+
+def generate_answer(sequence):
+    sentences = []
+    for i in range(0, len(sequence) - 1, 2):
+        sentences.append(construct_sequence(sequence[i], sequence[i + 1], sequence[i + 2]))
+    return ". ".join(sentences)
+
+
+def compare_statements(llm: LlmConnector, statement1: str, statement2: str):
+    request = f'Ответь односложно: "Да" или "Нет". Эквиваленты ли 2 утверждения "{statement1}" и "{statement2}"?'
+    response = llm.generate_response(request)
+    return response.lower().startswith("да")
+
+
+def check_answer(
+    ontologies: OntologiesConnector,
+    llm: LlmConnector,
+    domain: str,
+    source: str,
+    destination: str,
+    answer: str):
+    for length in range(MIN_LENGTH_TO_CHECK, MAX_LENGTH_TO_CHECK):
+        sequences = ontologies.get_path_sequences(domain, source, destination, length)
+        for sequence in  sequences:
+            reference = generate_answer(sequence)
+            if (compare_statements(llm, answer, reference)):
+                return True
+    return False
